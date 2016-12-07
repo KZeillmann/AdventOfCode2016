2508625120737898d8d1af6530b9cc1b63c70ffa
KZeillmann <kzeillmann@gmail.com>
2016-12-06 22:19:18 -0600

    Complete day 3 part 1

---
+++ 3.exs
@@ -0,0 +1,24 @@
+defmodule AdventThree do
+  def read_file(file) do
+    File.read!(file)
+    |> String.split("\n", trim: true)
+    # Rewrite following line -- it's nearly inscrutable
+    |> Enum.map(&(String.split(&1) |> Enum.map(fn(x) -> Integer.parse(x) |> elem(0) end)))
+  end
+
+  def solve_puzzle do
+    read_file("3.txt")
+    |> Enum.map(&(is_triangle?(Enum.at(&1, 0), Enum.at(&1, 1), Enum.at(&1, 2))))
+    |> Enum.reduce(&(&1+&2))
+  end
+
+  def is_triangle?(a, b, c) do
+    if a + b > c && a + c > b && b + c > a do
+      1
+    else
+      0
+    end
+  end
+end
+
+IO.inspect AdventThree.solve_puzzle
