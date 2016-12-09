f7045a424a4a418a41e4fb5f44faef872cdd4183
KZeillmann <kzeillmann@gmail.com>
2016-12-08 22:36:03 -0600

    Day 3: Part 2 complete

--- 3.exs
+++ 3.exs
@@ -8,6 +8,10 @@ defmodule AdventThree do
 
   def solve_puzzle do
     read_file("3.txt")
+    |> List.zip
+    |> Enum.map(&(Tuple.to_list(&1)))
+    |> List.flatten
+    |> Enum.chunk(3)
     |> Enum.map(&(is_triangle?(Enum.at(&1, 0), Enum.at(&1, 1), Enum.at(&1, 2))))
     |> Enum.reduce(&(&1+&2))
   end
